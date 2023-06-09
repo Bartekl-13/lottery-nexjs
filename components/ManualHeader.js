1730b5ff5916f9ad219c3fe6a684f808abf8ebe8
root <root@BARTEK-PC>
2023-06-09 12:02:50 +0200

    commit

---
+++ components/ManualHeader.js
@@ -0,0 +1,29 @@
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
+
+export default function ManualHeader() {
+    const { enableWeb3, account } = useMoralis()
+
+    useEffect(() => {
+        console.log("HI!")
+        console.log(isWeb3Enabled)
+    }, [])
+
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                    }}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
+}
